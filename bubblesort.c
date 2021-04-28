101512fa330085b8d6ec4ab64c1b7c840231ffd0
Sadhik Shaik <68614048+Sadhik0905@users.noreply.github.com>
2021-04-28 21:59:51 +0530

    To reduce comparisons
    In this I used flag variable to check whether if the array is sorted before all the comparisons. It helps the faster execution...!!!

--- bubblesort.c
+++ bubblesort.c
@@ -2,7 +2,7 @@
 
 int main(){
 
-   int count, temp, i, j, number[30];
+   int count, temp, i, j, number[30],flag=-1;
 
    printf("How many numbers are u going to enter?: ");
    scanf("%d",&count);
@@ -15,8 +15,12 @@ int main(){
    /* This is the main logic of bubble sort algorithm 
     */
    for(i=count-2;i>=0;i--){
+      if(flag==0)
+         break;
       for(j=0;j<=i;j++){
+         flag = 0;
         if(number[j]>number[j+1]){
+           flag = 1;
            temp=number[j];
            number[j]=number[j+1];
            number[j+1]=temp;
